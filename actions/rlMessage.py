74cc62957d091c88f793266916bb49f910cb4fcc
SL1225 <54695986+SL1225@users.noreply.github.com>
2022-03-12 14:30:33 +0800

    Update rlMessage.py

--- actions/rlMessage.py
+++ actions/rlMessage.py
@@ -60,7 +60,7 @@ class RlMessage:
             'content': f'[{Utils.getAsiaDate()} {Utils.getAsiaTime()}]{msg}',
         }
         headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:92.0) Gecko/20100101 Firefox/92.0'
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:92.0) Gecko/20100101 Firefox/92.0',
             "Content-Type": "application/json"
         }
         res = requests.post("http://www.pushplus.plus/send", headers=headers, json=params)
